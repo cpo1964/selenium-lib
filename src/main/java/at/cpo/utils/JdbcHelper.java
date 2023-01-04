81171a5e663e0c2966258cd620d4fb4911f3ef77
cpo1964 <cpo1964@aon.at>
2023-01-04 13:57:40 +0100

    refactored

--- src/main/java/at/cpo/utils/JdbcHelper.java
+++ src/main/java/at/cpo/utils/JdbcHelper.java
@@ -1,22 +1,37 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Elias Nogueira
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package at.cpo.utils;
-
-//import org.apache.logging.log4j.LogManager;
-//import org.apache.logging.log4j.Logger;
 
 import java.sql.*;
 import java.util.HashMap;
 
-// TODO: Auto-generated Javadoc
 /**
  * The type Jdbc helper.
  */
 public class JdbcHelper {
-    /**
-     * The Constant LOGGER.
-     */
-//    private static final Logger LOGGER = LogManager.getLogger(JdbcHelper.class);
 
-    /**
+	/**
      * The Url.
      */
     static final String URL = "url";
@@ -89,8 +104,7 @@ public class JdbcHelper {
         try {
             con = DriverManager.getConnection(map.get(URL), map.get(USERNAME), map.get(PASSWORD));
         } catch (SQLException ex) {
-            // log an exception. fro example:
-//            LOGGER.debug("Failed to create the database connection.");
+            // log an exception ...
         }
         return con;
     }
--- src/main/java/at/cpo/utils/RestHelper.java
+++ src/main/java/at/cpo/utils/RestHelper.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Elias Nogueira
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package at.cpo.utils;
 
 import org.apache.http.*;
@@ -27,7 +50,6 @@ import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.logging.Logger;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class RestHelper.
  */
@@ -52,7 +74,7 @@ public class RestHelper {
     static {
         if (httpclient == null) {
             try {
-                // ein client ohne proxy und ohne definierten CookieStore wird der BasicCookieStore verwendet
+                // client without proxy and no defined CookieStore uses BasicCookieStore
                 httpclient = buildHttpClient(null, null);
             } catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e) {
                 httpclient = null;
